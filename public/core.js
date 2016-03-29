31a189d63b9da86e101143b88527327f130b8967
Heng Xu <xu.he@husky.neu.edu>
2016-03-29 14:56:25 -0700

    created angular module & controller for foodapp

--- public/core.js
+++ public/core.js
@@ -1,3 +1,42 @@
-/**
- * Created by hengxu on 3/29/16.
- */
+var foodApp = angular
+    .module('foodApp', []);
+
+function mainController($scope, $http) {
+    $scope.formData = {};
+
+    // when landing on the page, get all food and show them
+    $http.get('/api/food')
+        .success(function(data) {
+            $scope.food = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    // when submitting the add form, send the text to the node API
+    $scope.createFood = function() {
+        $http.post('/api/food', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {}; // clear the form so our user is ready to enter another
+                $scope.food = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    // delete a food after checking it
+    $scope.deleteFood = function(id) {
+        $http.delete('/api/food/' + id)
+            .success(function(data) {
+                $scope.food = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+}
